105cd079d5069f13e7e4b2e5f5ea87113266665d
Ian McCormack <icmccorm@andrew.cmu.edu>
2024-05-23 15:41:11 -0400

    Removed unnecessary lifetime bounds.

--- examples/cyclic/src/main.rs
+++ examples/cyclic/src/main.rs
@@ -12,23 +12,18 @@ pub struct Parent<'p, 'c> {
     pub child: Option<(Pointer<'c>, Write<'c, Child<'p>>, Dealloc<'c, Child<'p>>)>,
 }
 
-pub fn init<'p, 'c>(parent: Pointer<'p>, perm: &Write<'p, Parent<'p, '_>>)
-where
-    'p: 'c,
-{
+pub fn init<'p>(parent: Pointer<'p>, perm: &Write<'p, Parent<'p, '_>>) {
     let child_allocation = Value::new(Child {
         child_data: 0,
         parent,
     });
+
     let (child, child_perm, child_dealloc) = child_allocation.into_raw();
 
     parent.as_mut(perm).child = Some((child, child_perm, child_dealloc));
 }
 
-pub fn deinit<'p, 'c>(parent: Pointer<'p>, perm: &Write<'p, Parent<'p, 'c>>)
-where
-    'p: 'c,
-{
+pub fn deinit<'p>(parent: Pointer<'p>, perm: &Write<'p, Parent<'p, '_>>) {
     let (child, child_perm, child_dealloc) = parent.as_mut(perm).child.take().unwrap();
     drop(Value::from_raw(child, child_perm, child_dealloc));
 }
